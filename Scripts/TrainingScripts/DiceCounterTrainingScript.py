664fc7640b1b76becf1cf006d6a6b1a4677be7b7
TJMazerolle <tmazerol@uwaterloo.ca>
2024-08-21 12:53:48 -0400

    Put coin and dice scripts together and updated directories

---
+++ Scripts/TrainingScripts/DiceCounterTrainingScript.py
@@ -0,0 +1,11 @@
+import os
+import sys
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append('./HelperScripts')
+from YOLOModelTraining import *
+
+check_GPU()
+
+if __name__ == '__main__':
+    model = train_YOLO_model(training_epochs = 1000)
+    move_created_model("DiceDetectionModel.pt")
